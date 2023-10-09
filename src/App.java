7b448f1368972c6ad8a1fc5592c75a730bd72d29
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-09 08:27:41 +0200

    skriver ut om tal1 o tal2 är lika, mindre, större

--- src/App.java
+++ src/App.java
@@ -2,6 +2,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        Scanner tb = new Scanner(System.in);
+
+        System.out.print("Skriv in ett tal: ");
+        int tal1 = tb.nextInt();
+
+        System.out.print("Skriv in ett till tal: ");
+        int tal2 = tb.nextInt();
+
+        System.out.println("Är tal1 och tal2 lika stora?: "+(tal1==tal2));
+        System.out.println("Är tal1 mindre än tal2?: "+(tal1<tal2));
+        System.out.println("Är tal1 större än tal2?: "+(tal1>tal2));
+
+
+
+
+
         
     }
 
