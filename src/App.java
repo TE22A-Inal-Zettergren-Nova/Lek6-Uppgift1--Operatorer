327a4844f8cee887581cffd672deeb540f5b68a6
Nova Inal Zettergren <nova.inalzettergren@elev.ga.ntig.se>
2023-10-09 08:52:17 +0200

    jämför elementen i fältet med varandra

--- src/App.java
+++ src/App.java
@@ -4,6 +4,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         Scanner tb = new Scanner(System.in);
 
+        /*Uppgift a */
+
         System.out.print("Skriv in ett tal: ");
         int tal1 = tb.nextInt();
 
@@ -14,8 +16,16 @@ public class App {
         System.out.println("Är tal1 mindre än tal2?: "+(tal1<tal2));
         System.out.println("Är tal1 större än tal2?: "+(tal1>tal2));
 
+        /*Uppgift b */
+
+        int[] talen ={4,5,2};
+        System.out.println(" ");
+        System.out.println("Är "+talen[0]+" större än "+talen[1]+" eller "+talen[2]+"?: "+(talen[0]>talen[1] || talen[0]>talen[2]));
+        System.out.println("Är "+talen[0]+" mindre än "+talen[1]+" eller "+talen[2]+"?: "+(talen[0]<talen[1] || talen[0]<talen[2]));
+        System.out.println("Är "+talen[0]+" lika med "+talen[1]+" eller "+talen[2]+"?: "+(talen[0]>talen[1] || talen[0]>talen[2]));
 
 
+        
 
 
         
